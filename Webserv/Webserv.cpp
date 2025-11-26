08b04409dd5251323a0b9936d97c3c5f3d745629
Antonio Luis Moreno Del Pino <antonimo@c3r3s6.42malaga.com>
2025-11-26 14:11:22 +0100

    Add Webserv class
    - Create new class with standard constructors and destructors

---
+++ Webserv/Webserv.cpp
@@ -0,0 +1,5 @@
+#include "Webserv.hpp"
+
+Webserv::Webserv(void) {}
+
+Webserv::~Webserv(void) {}
---
+++ Webserv/Webserv.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+class Webserv {
+	private:
+
+	public:
+		Webserv(void);
+		~Webserv(void);
+};

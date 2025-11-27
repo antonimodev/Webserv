89f54a23451802704093bc0edf22ed5127314816
Antonio Luis Moreno Del Pino <antonimo@c3r2s3.42malaga.com>
2025-11-27 14:15:58 +0100

    Adjust bind_socket function

--- webserv.h
+++ webserv.h
@@ -4,6 +4,6 @@
 int     create_socket(int domain, int type, int protocol);
 void	set_nonblocking(int& socket_fd);
 void	set_socket_mode(int& socket_fd, int mode);
-void    bind_socket(int& socket_fd, const char* ip, int port);
+void    bind_socket(int& socket_fd, const char* ip, int port, int domain);
 void    listen_socket(int& socket_fd, int backlog);
 int		init_server_socket(void);
